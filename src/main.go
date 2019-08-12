42865d626001ad0e85ac7cbc3c976447a4e5a1e4
Leonardo <leonardo_7@live.com.ar>
2019-08-12 02:26:50 -0300

    pruebas

--- src/main.go
+++ src/main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 	_ "github.com/heroku/x/hmetrics/onload"
-
 )
 
-func main() {
+const (
+	STATIC_DIR = "/static/"
+	PORT       = "8080"
+)
+
+func main(){
+	myRouter := mux.NewRouter().StrictSlash(true)
+	//myRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/templates/home.tmpl.html")))
+	myRouter.HandleFunc("/", homePage).Methods("GET")
+	myRouter.HandleFunc("/game/player/:name", createNewGame).Methods("POST")
+	http.Handle("/", myRouter) // enable the router
+
+	myRouter.PathPrefix(STATIC_DIR)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+func push(w http.ResponseWriter, resource string) {
+	pusher, ok := w.(http.Pusher)
+	fmt.Print(ok)
+	if ok {
+		if err := pusher.Push(resource, nil); err == nil {
+			return
+		}
+	}
+}
+func createNewGame(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key := vars["name"]
+
+	fmt.Fprintf(writer, "Key: " + key)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	push(w,"/templates/home.tmpl.html")
+
+//	fmt.Fprintf(w, "Welcome to the HomePage!")
+//	fmt.Println("Endpoint Hit: homePage")
+
+}
+
+func main2() {
 	port := os.Getenv("PORT")
 	port = "5001"
 	if port == "" {
