19a8ff38d593b1c96fa42cb0442fecd9d2330065
Core <matiasarapura>
2019-08-13 00:32:58 -0300

    crear y unirse api

--- src/GamesRepository.go
+++ src/GamesRepository.go
@@ -4,20 +4,96 @@ import "sync"
 
 var once sync.Once
 
+
+type Entity interface {
+	ID() int
+}
+
+type Game struct {
+	ID int
+	Admin Player
+	Players []Player
+
+}
+
+type Player struct {
+	ID int
+	Name string
+
+}
+
+var games []Game
+
 // type global
-type singleton map[string]string
+type GamesRepository map[string]string
 
 var (
-	instance singleton
+	instance GamesRepository
+	currentGameId int
+	currentPlayerId int
 )
 
-func GamesRepository() singleton {
+func GetGamesRepository() GamesRepository {
 
 	once.Do(func() { // <-- atomic, does not allow repeating
 
-		instance = make(singleton) // <-- thread safe
-
+		instance = make(GamesRepository) // <-- thread safe
+		currentGameId= 1
+		currentPlayerId= 1
 	})
 
 	return instance
+}
+
+func (repo *GamesRepository) GetAllGames() []Game{
+	return games
+}
+
+func (repo *GamesRepository) GetGame(gameId int) Game {
+
+	for _, v := range games {
+		if v.ID==gameId {
+			return v
+		}
+	}
+	panic("id inexistente")
+}
+
+func (repo *GamesRepository) CreateGame(admin Player) int{
+	currentGameId++
+	newGame := Game{ID:currentGameId,Admin:admin,Players:make([]Player,10)}
+	games = append(games,newGame)
+	return currentGameId
+}
+
+
+func (game *Game) AddPlayer(player Player) Game {
+	game.Players = append(game.Players,player)
+	return *game
+}
+
+
+func createPlayer(playerName string)Player  {
+	currentPlayerId++
+	player := Player{
+		ID:   currentPlayerId,
+		Name: playerName,
+	}
+	return player
+}
+
+
+
+
+
+
+
+func Filter(vs []Game, f func(Game) bool) []Game {
+	vsf := make([]Game, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
 }
--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
@@ -16,7 +17,7 @@ const (
 	PORT       = "8080"
 )
 
-func main(){
+func main2(){
 	myRouter := mux.NewRouter().StrictSlash(true)
 	//myRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("/templates/home.tmpl.html")))
 	myRouter.HandleFunc("/", homePage).Methods("GET")
@@ -50,12 +51,13 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 }
 
-func main2() {
+func main() {
 	port := os.Getenv("PORT")
 	port = "5001"
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+	repo := GetGamesRepository()
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -68,12 +70,39 @@ func main2() {
 	router.GET("/home", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.tmpl.html", nil)
 	})
+	router.GET("/games", func(c *gin.Context) {
+		c.JSON(http.StatusOK,gin.H{"status": http.StatusOK, "data": repo.GetAllGames()})
 
-	router.Run(":" + port)
+	})
 
-	GamesRepository()
+
+	player :=createPlayer("jhon")
+
+	gid:=repo.CreateGame(player)
+	game := repo.GetGame(gid)
+
+	fmt.Println(game.AddPlayer(player))
+	fmt.Println(game.AddPlayer(createPlayer("pita")))
+	fmt.Println(game.Players)
+
+
 
 	router.POST("/game/player/:name", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "home.tmpl.html", 43535)
+		adminName := context.Param("name")
+		player :=createPlayer(adminName)
+
+		context.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": repo.CreateGame(player)} )
 	})
+
+	router.POST("/games/:id/players/:name", func(context *gin.Context) {
+
+		player:=createPlayer(context.Param("name"))
+		fmt.Println(player)
+		id, _ := strconv.Atoi(context.Param("id"))
+		game := repo.GetGame(id)
+		context.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": game.AddPlayer(player)} )
+	})
+
+
+	router.Run(":" + port)
 }
